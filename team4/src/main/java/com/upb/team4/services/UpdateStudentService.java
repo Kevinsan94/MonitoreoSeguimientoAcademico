167b58bb1543aedc1a0350bd541d09848f79e2f2
Kevinsan94 <kevinviscafe@gmail.com>
2018-06-03 20:21:05 -0400

    Update student

---
+++ team4/src/main/java/com/upb/team4/services/UpdateStudentService.java
@@ -0,0 +1,31 @@
+package com.upb.team4.services;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.upb.team4.beans.Usuario;
+import com.upb.team4.utils.AuthUtils;
+import com.upb.team4.utils.DBUtils;
+
+@Path("/update")
+public class LoginService {
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.TEXT_PLAIN})
+    public Response validaUsuario(Usuario u) {
+
+        String response = authenticate(u.getUsername(),u.getPassword());
+
+        return Response.ok(response).build();
+    }
+
+    @GET
+    public Response test(){
+        return Response.ok("OK").build();
+    }
+}
